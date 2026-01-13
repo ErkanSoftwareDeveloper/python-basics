e3d0a498b95644526f5e6aca657e64b332c0f7a9
Erkan <erkansoftwaredeveloper@gmail.com>
2026-01-13 18:58:30 +0100

    Add files via upload

---
+++ 1-variables/variables.py
@@ -0,0 +1,19 @@
+# Python Variables Examples
+
+
+# 1. Basic variables
+name = "Erkan"    # String variable
+age = 25          # Integer variable
+height = 1.75  # Float variable
+is_student = True  # Boolean variable
+
+""" String (str) = Stores text, like words or sentences.
+
+Integer (int) = Stores whole numbers, without decimals.
+
+Float (float) = Stores numbers with decimals.
+
+Boolean (bool) = Stores True or False, used for conditions."""
+
+# printing variables
+print("Name:", name)
