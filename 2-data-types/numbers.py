18a463bfd0663a58490552f51298700d278e4dc4
Erkan <erkansoftwaredeveloper@gmail.com>
2026-01-15 21:03:16 +0100

    Update numbers.py

---
+++ 2-data-types/numbers.py
@@ -0,0 +1,25 @@
+"""
+
+Numbers in Python
+
+In Python, numbers are used to perform mathematical operations.
+The most common numeric data types are int and float.
+
+int: whole numbers
+float: decimal numbers
+
+"""
+
+# Practise int
+a = 10
+b = -4
+print(a)
+print(b)
+
+# Practise float
+pi = 3.14
+price = 2.5
+print(pi)
+print(price)
+
+# Basic Math Operations
