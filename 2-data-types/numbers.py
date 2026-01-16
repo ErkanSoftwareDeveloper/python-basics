26ba83ec56aa33ba8964de332dc132f455a6f6da
Erkan <erkansoftwaredeveloper@gmail.com>
2026-01-16 22:03:37 +0100

    Update numbers.py

--- 2-data-types/numbers.py
+++ 2-data-types/numbers.py
@@ -23,3 +23,35 @@ print(pi)
 print(price)
 
 # Basic Math Operations
+x = 10
+y = 3
+
+# Addition
+print("Addition:", x + y) # 13
+
+# Subtraction
+print("Subtraction:", x - y) # 7
+
+# Multiplication
+print("Multiplication:", x * y) # 30
+
+# Division
+print("Division:", x / y) # 3.333... 
+
+# Integer Division
+print("Integer Division:", x // y) # 3 (Divides two numbers and keeps only the whole part, 10 / 3 = 3.3333 // = 3 )
+
+# Modulus (remainder)
+print("Modulus:", x % y) # 1 (Gives the remainder after division)
+
+# Exponentiation
+print("Exponentiation:", x ** y) # 1000e (Raises a number to a power) 2 ** 3 = 8 (2*2*2 = 8)
+
+# Type Conversion
+x = 10 # int
+y = float(x) # 10.8
+z = int(3.99) # 3
+print(x, y, z) # 10 10.0 3
+
+# Built in Math Functions
+
