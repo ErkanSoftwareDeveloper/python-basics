0f45cca15ea169af63bf8b9a5b1da220baee3708
Erkan <erkansoftwaredeveloper@gmail.com>
2026-01-18 21:29:48 +0100

    Update strings.py

---
+++ 2-data-types/strings.py
@@ -0,0 +1,25 @@
+""" A string is a sequence of characters, used to represent text.
+
+Strings are written inside quotes: single ' ' or double " ".
+
+Examples: """
+
+# String Concatenation 
+first = "Hello"
+second = "World"
+message = first + " " + second
+print(message) # Hello World
+
+# Multiplication
+laugh = "Ha"
+print(laugh * 3) #HaHaHa
+
+# Indexing Strings
+word = "Python"
+print(word[0]) # P = first charachter
+print(word[3]) # H = fourth charahter
+# Negative indexing counts from the end:
+print(word[0]) # n = last character
+print(word[-2] # o = second to last
+
+# Slicing Strings
