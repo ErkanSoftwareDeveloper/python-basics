cdacff670c0bb3e8dcdcbaa9fca664e742c44310
Erkan <erkansoftwaredeveloper@gmail.com>
2026-01-25 12:56:36 +0100

    Update dictionaries.py

--- 2-data-types/dictionaries.py
+++ 2-data-types/dictionaries.py
@@ -3,3 +3,22 @@
 Instead of indexes (0, 1, 2), you use keys.
 
 Dictionaries are written using curly braces {} """
+
+# Dictionaries in Python
+person = {
+  "name": "Erkan",
+  "age": 25,
+  "is_student": False
+}
+
+# Accessing Values
+print(person) # 
+print(person["name]") # Erkan
+print(person["age]") # 25
+
+# using (get) safe way!
+print(person.get("name")) # Erkan
+print(person.get("job")) # None
+print(person.get("job", "N/A")) # N/A
+
+# adding new key-value pairs
