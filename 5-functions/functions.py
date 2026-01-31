004269203199e313102d343c1f8f9ef7298f6041
Erkan <erkansoftwaredeveloper@gmail.com>
2026-01-31 16:01:51 +0100

    Update functions.py

---
+++ 5-functions/functions.py
@@ -0,0 +1,17 @@
+""" What is a Function?
+
+A function is a reusable block of code.
+
+Instead of writing the same code again and again, you:
+
+write it once
+
+call it whenever you need it
+
+Think of it like a machine:
+
+you give it input
+
+it does something
+
+it may give you output """
