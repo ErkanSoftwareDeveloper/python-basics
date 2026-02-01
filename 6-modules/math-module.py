db42f07cdaf775426bf4d6e693f5d9e03c76a945
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-01 11:22:07 +0100

    Update math-module.py

---
+++ 6-modules/math-module.py
@@ -0,0 +1,8 @@
+""" What is a Module?
+
+A module is a file that contains ready-made code (functions, variables).
+
+Python already comes with many built-in modules.
+math is one of them.
+
+👉 Instead of writing math logic yourself, you import it. """
