846e0870d09b7bb52287583b69069bdfb92b40bb
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-04 09:21:36 +0100

    Add file-read-write.py for file operations overview

---
+++ 7-file-operations/file-read-write.py
@@ -0,0 +1,20 @@
+""" File Read & Write in Python (file-read-write.py)
+️ What does “file read / write” mean?
+
+It means:
+
+Read data from a file
+
+Write data into a file
+
+Save information permanently
+
+Example use cases:
+
+saving user data
+
+logs
+
+config files
+
+simple databases """
