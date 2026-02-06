34652375e2006340c9fb50e8425507c508f5b9dc
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-06 19:50:13 +0100

    Document common Python errors in try-except.py
    Add a docstring explaining common Python errors.

---
+++ 8-errors/try-except.py
@@ -0,0 +1,12 @@
+""" What is an Error?
+
+An error is when Python cannot run your code. 
+
+ZeroDivisionError	  10 / 0
+NameError	          print(x) if x is undefined
+TypeError	          5 + "5"
+IndexError	        [1,2][5]
+KeyError	          {"a":1}["b"] 
+
+"""
+
